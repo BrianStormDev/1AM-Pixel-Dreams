eb46b672203cebb38e5e317a8c5e83517a9f134f
BrianStormYT <66709977+BrianStormYT@users.noreply.github.com>
2022-07-23 17:43:52 -0700

    Added Art Mobs
    Scattered art mobs in the level and also squished a few angel mob bugs

--- 1AM/Scripts/Weeping Angel.gd
+++ 1AM/Scripts/Weeping Angel.gd
@@ -1,24 +1,24 @@
 extends KinematicBody2D
 
 const GRAVITY = 110
-var damage = 60
+var damage = 50
 var velocity = Vector2()
 export var direction = 1
 export var health = 150
 export var speed = 0
-const value = 750
+const value = 1000
 signal dead
-
 
 func dead():
 	speed = 0
 	set_collision_layer_bit(4, false) 
 	$Area2D.set_monitoring(false)
 	emit_signal("dead", value)
+	queue_free()
 	
 func _ready(): 
 	if direction == 1:
-		scale.x = -scale.x
+		$Sprite.flip_h = !$Sprite.flip_h
 
 func _physics_process(delta):
 	if health > 0:
@@ -29,10 +29,10 @@ func _physics_process(delta):
 		dead()
 		
 	if $LeftDetect.is_colliding() && $LeftDetect.get_collider().facing_right:
-		speed = -500
+		speed = 500 * -direction
 		$Sprite.set_frame(2)
 	elif $RightDetect.is_colliding() && !$RightDetect.get_collider().facing_right:
-		speed = 500
+		speed = -500 * -direction
 		$Sprite.set_frame(1)
 	else:
 		speed = 0
