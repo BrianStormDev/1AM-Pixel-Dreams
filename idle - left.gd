7c20c71d197a7daaa71a74f252da470a377c5e67
BobDaDino <108113366+BobDaDino@users.noreply.github.com>
2022-08-08 22:13:19 -0700

    Minotaur Mob implementation

---
+++ idle - left.gd
@@ -0,0 +1,8 @@
+extends KinematicBody2D
+
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	$AnimationPlayer.play("moving left");
+	pass # Replace with function body.
